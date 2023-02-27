347e9ad2639ad9d9f4ffae5c64f9557e7ea775b8
aihamrad <37958382+aihamrad@users.noreply.github.com>
2023-02-27 16:55:18 +0800

    remove unnecessary import

--- routes/history.js
+++ routes/history.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const historyController = require('../controllers/historyController')
-var http = require('http')
 const router = express.Router();
 
 router.get('/', historyController.getHistroy);
